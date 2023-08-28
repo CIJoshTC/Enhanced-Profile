8d2eb86b2bc3914c56b906c40c182c262880f5af
Joshua <23joshtc@gmail.com>
2023-08-28 19:51:41 -0400

    header.js updated

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
 import About from './components/About'
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
        <Header /> {}
        <About /> {}
+       <Projects /> {}
       <header className="App-header">
       
       </header>

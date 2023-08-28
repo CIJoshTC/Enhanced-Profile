dc9630f0958fc806fa3290395afe89a6ef99377a
Joshua <23joshtc@gmail.com>
2023-08-27 21:36:16 -0400

    Created Abut css component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
        <Header /> {}
+       <About /> {}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

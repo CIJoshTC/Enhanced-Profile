2a18d1e78f863391dc8100ead5f6dfecbc903dbd
Joshua <23joshtc@gmail.com>
2023-08-28 19:09:27 -0400

    the projects tmeplate has been created

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -4,15 +4,16 @@ import './Header.css';
 const Header = ()=> {
     return (
         <header className = "header">
-        <nav className="nav">
-        <div className="logo">Joshua Thomas Connell</div>
-        <ul className="nav-list">
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contacts</a></li>
-        </ul>
-      </nav>
-    </header>
+         <nav className="nav">
+          <div className="logo">Joshua Thomas Connell</div>
+            <ul className="nav-list">
+              <li><a href="#about">About</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+         </nav>
+     
+        </header>
   );
 };
 
---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './projects.css';
+
+const projectsData = [
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+
+    },
+
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+    
+
+    }
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+
+    }
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+
+    }
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+
+    }
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+
+    }
+]

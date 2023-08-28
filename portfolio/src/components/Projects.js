50120329021d4c5ae72c117537bb70631307db2b
Joshua <23joshtc@gmail.com>
2023-08-28 19:17:17 -0400

    the projects componenet complete

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -19,7 +19,31 @@ const projectsData = [
         githubUrl: 'https://github.com/project1-repo',
     
 
-    }
+    },
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+
+    },
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+
+    },
+    {
+        title: 'Project 1',
+        description: 'My first project.',
+        imageUrl: 'project1-image-url.jpg',
+        demoUrl: 'https://project1-demo.com',
+        githubUrl: 'https://github.com/project1-repo',
+
+    },
     {
         title: 'Project 1',
         description: 'My first project.',
@@ -28,28 +52,28 @@ const projectsData = [
         githubUrl: 'https://github.com/project1-repo',
 
     }
-    {
-        title: 'Project 1',
-        description: 'My first project.',
-        imageUrl: 'project1-image-url.jpg',
-        demoUrl: 'https://project1-demo.com',
-        githubUrl: 'https://github.com/project1-repo',
+];
 
-    }
-    {
-        title: 'Project 1',
-        description: 'My first project.',
-        imageUrl: 'project1-image-url.jpg',
-        demoUrl: 'https://project1-demo.com',
-        githubUrl: 'https://github.com/project1-repo',
+const Projects = () => {
+    return (
+    <section id= "projects" classname="projects">
+         <h2>Projects</h2>
+         <div classname= "projects-list">
+         {projectsData.map((project, index) => (
+             <div className="project" key={index}>
+             <img src={project.imageUrl} alt={project.title} />
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <div className="project-links">
+                <a href={project.demoUrl} target="_blank" rel="noopener noreferrer">Demo</a>
+                <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">GitHub</a>
+              </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-    }
-    {
-        title: 'Project 1',
-        description: 'My first project.',
-        imageUrl: 'project1-image-url.jpg',
-        demoUrl: 'https://project1-demo.com',
-        githubUrl: 'https://github.com/project1-repo',
 
-    }
-]
+export default Projects;
